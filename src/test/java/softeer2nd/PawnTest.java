0518e3643b8652303508b2b14639a6781132082a
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-03 17:39:25 +0900

    [feat]: 1. Pawn 테스트 완료

---
+++ src/test/java/softeer2nd/PawnTest.java
@@ -0,0 +1,23 @@
+package softeer2nd;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PawnTest {
+
+
+    @Test
+    @DisplayName("Pawn Create Validation")
+    void create() {
+        //흰색 폰
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+        //검은 폰
+        Pawn pawn2 = new Pawn("black");
+        assertThat(pawn2.getColor()).isEqualTo("black");
+    }
+
+
+}
