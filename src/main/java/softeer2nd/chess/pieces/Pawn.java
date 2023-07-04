cbbca093e39c23cc2162bcb8ef02546ec8b29412
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-04 13:46:21 +0900

    [feat] : 1. Pawn 색에 따른 출력 문자 부여 완료

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -5,15 +5,36 @@ public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "BLACK";
 
-    private final String color;
-    private String[] location;
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
 
-    public Pawn() {this.color = WHITE_COLOR;}
+    private final String color;
+    private String location;
+
+    public Pawn() {
+        this.color = WHITE_COLOR;
+    }
     public Pawn(String color) {
         this.color = color;
     }
 
+    public Pawn(String color, String location) {
+        this.color = color;
+        this.location = location;
+    }
+
     public String getColor() {
         return this.color;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public char getRepresentation() {
+        if(this.color == WHITE_COLOR) {
+            return WHITE_REPRESENTATION;
+        }
+        return BLACK_REPRESENTATION;
     }
 }
--- src/test/java/softeer2nd/chess/pieces/PawnTest.java
+++ src/test/java/softeer2nd/chess/pieces/PawnTest.java
@@ -13,21 +13,24 @@ public class PawnTest {
     @Test
     @DisplayName("Pawn Create Validation")
     void create() {
-        verifyPawn(Pawn.WHITE_COLOR);
-        verifyPawn(Pawn.BLACK_COLOR);
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    private static void verifyPawn(final String color) {
+    private static void verifyPawn(final String color, final char representation) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+        assertEquals(representation, pawn.getRepresentation());
     }
 
     @Test
     @DisplayName("Pawn Default Constructor Validation")
     void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals("white", pawn.getColor());
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
+
 
 
 }
