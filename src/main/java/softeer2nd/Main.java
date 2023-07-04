67197cec13280af05e1ec509c2b6264ca65f2503
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-07-04 17:17:31 +0900

    [feat] : main() 구현 완료

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -1,7 +1,25 @@
 package softeer2nd;
 
+import softeer2nd.chess.Board;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        while(true) {
+            String runChess = scan.nextLine();
+            switch (runChess) {
+                case "start":
+                    Board board = new Board();
+                    board.initialize();
+                    System.out.println(board.print());
+                    break;
+                case "end":
+                    scan.close();
+                    return;
+            }
+        }
+
     }
 }
